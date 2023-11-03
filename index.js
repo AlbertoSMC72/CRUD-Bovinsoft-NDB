01efb9291c3e8dc900ccf5face87d71cf4e9da1b
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-03 10:49:40 -0600

    modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bovinosRouter = require('./src/routes/bovinos.route')
 const estadosRouter = require ("./src/routes/estados.route")
 const eventosRouter = require('./src/routes/evetos.route')
+const administradoresRouter = require('./src/routes/administradores.route')
 
 
 const app = express()
@@ -10,6 +11,8 @@ app.use(express.json())
 app.use('/bovinos', bovinosRouter)
 app.use("/estados",estadosRouter)
 app.use('/eventos', eventosRouter)
+app.use('/administradores', administradoresRouter)
+
 
 
 const PORT = process.env.PORT
