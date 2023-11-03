d46f81bbf0ade7031135428421c0d99e9803a097
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-11-03 10:20:45 -0600

    modified:   index.js 	new file:   src/controllers/administradores.controller.js 	new file:   src/routes/administradores.route.js 	modified:   src/routes/evetos.route.js aqui se creo l

--- src/routes/evetos.route.js
+++ src/routes/evetos.route.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const authMiddleware = require('../middlewares/auth.middleware')
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/auth.middleware');
+const eventosController = require('../controllers/eventos.controller');
 
-const eventosController = require('../controllers/eventos.controller')
+router.get('/', authMiddleware.verificarJWT, eventosController.index);
+router.get('/:id', authMiddleware.verificarJWT, eventosController.getById);
+router.post('/', authMiddleware.verificarJWT, eventosController.create);
+router.put('/:id', authMiddleware.verificarJWT, eventosController.update);
+router.patch('/logical-delete/:id', authMiddleware.verificarJWT, eventosController.deleteLogico);
+router.delete('/physical-delete/:id', authMiddleware.verificarJWT, eventosController.deleteFisico);
 
-
-router.get('/', authMiddleware.verificarJWT, eventosController.index)
-router.get('/:id', authMiddleware.verificarJWT, eventosController.getById)
-router.post('/', authMiddleware.verificarJWT, eventosController.create)
-router.patch('/:id', authMiddleware.verificarJWT, eventosController.updateParcial)
-router.put('/:id', authMiddleware.verificarJWT, eventosController.updateCompleto)
-router.delete('/:id', authMiddleware.verificarJWT, eventosController.delete)
-
-module.exports = router
+module.exports = router;
