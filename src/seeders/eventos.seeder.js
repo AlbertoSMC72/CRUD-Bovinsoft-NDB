f8aa8f8e10fba0ea054782d8205180d0d95e7034
ATRIZEL203 <darkangel_066@hotmail.com>
2023-11-04 12:12:25 -0600

    no message

--- src/seeders/eventos.seeder.js
+++ src/seeders/eventos.seeder.js
@@ -1,31 +1,48 @@
-const db = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+};
+
+
+console.log('HOST:', process.env.HOST);
+console.log('USER:', process.env.USER);
+console.log('PASSWORD:', process.env.PASSWORD);
+console.log('DATABASE:', process.env.DATABASE);
 
 const eventos = [
   {
-    idBovino: 1, 
-    titulo: 'Evento 1', 
-    asunto: 'Asunto 1', 
-    fecha_Reporte: '2023-11-10', 
-    descripcion: 'Descripción del evento 1',
-    fecha_Reinsidio: '2023-11-11', 
-  }
-
+    idBovino: 1,
+    titulo: "Evento 1",
+    asunto: "Asunto 1",
+    fecha_Reporte: "2023-11-02",
+    descripcion: "Descripción del evento 1",
+    fecha_Reinsidio: "2023-11-03",
+    eventoTerminado: 0,
+  },
+  // Agrega más eventos aquí
 ];
 
-const insertarEventos = async () => {
-  const connection = await db.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD, 
-    database: process.env.DATABASE, 
-    port: process.env.PORT, 
-  });
-
+async function insertarEventos() {
   try {
+    const connection = await mysql.createConnection(dbConfig);
+
     for (const evento of eventos) {
-      await connection.execute(
+      // Verifica que los campos no sean undefined y reemplaza con null si es necesario
+      for (const key in evento) {
+        if (evento.hasOwnProperty(key) && evento[key] === undefined) {
+          evento[key] = null;
+        }
+      }
+
+      const [results] = await connection.execute(
         'INSERT INTO Eventos (idBovino, titulo, asunto, fecha_Reporte, descripcion, fecha_Reinsidio, eventoTerminado) VALUES (?, ?, ?, ?, ?, ?, ?)',
         [
           evento.idBovino,
@@ -37,14 +54,14 @@ const insertarEventos = async () => {
           evento.eventoTerminado,
         ]
       );
+      console.log(`Evento insertado con ID: ${results.insertId}`);
     }
 
-    console.log('Eventos insertados correctamente');
+    await connection.end();
   } catch (error) {
     console.error('Error al insertar eventos:', error);
-  } finally {
-    connection.end(); 
   }
-};
+}
 
 insertarEventos();
+
